db757ddab14f4b3ded7028151c4ec00cc3a5ed30
schaepher <schaepher@outlook.com>
2023-07-02 16:39:04 +0800

    wait for images

--- converter/doctron_core/html2pdf.go
+++ converter/doctron_core/html2pdf.go
@@ -7,54 +7,55 @@ import (
 
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/kb"
 )
 
-//DefaultLandscape Paper orientation. Defaults to false.
+// DefaultLandscape Paper orientation. Defaults to false.
 const DefaultLandscape = false
 
-//DefaultDisplayHeaderFooter Display header and footer. Defaults to false.
+// DefaultDisplayHeaderFooter Display header and footer. Defaults to false.
 const DefaultDisplayHeaderFooter = false
 
-//DefaultPrintBackground Print background graphics. Defaults to true.
+// DefaultPrintBackground Print background graphics. Defaults to true.
 const DefaultPrintBackground = true
 
-//DefaultScale Scale of the webpage rendering. Defaults to 1.
+// DefaultScale Scale of the webpage rendering. Defaults to 1.
 const DefaultScale = 1
 
-//DefaultPaperWidth Paper width in inches. Defaults to 8.5 inches.
+// DefaultPaperWidth Paper width in inches. Defaults to 8.5 inches.
 const DefaultPaperWidth = 8.5
 
-//DefaultPaperHeight Paper height in inches. Defaults to 11 inches.
+// DefaultPaperHeight Paper height in inches. Defaults to 11 inches.
 const DefaultPaperHeight = 11
 
-//DefaultMarginTop Top margin in inches. Defaults to 1cm (~0.4 inches).
+// DefaultMarginTop Top margin in inches. Defaults to 1cm (~0.4 inches).
 const DefaultMarginTop = 0.4
 
-//DefaultMarginBottom Bottom margin in inches. Defaults to 1cm (~0.4 inches).
+// DefaultMarginBottom Bottom margin in inches. Defaults to 1cm (~0.4 inches).
 const DefaultMarginBottom = 0.4
 
-//DefaultMarginLeft Left margin in inches. Defaults to 1cm (~0.4 inches).
+// DefaultMarginLeft Left margin in inches. Defaults to 1cm (~0.4 inches).
 const DefaultMarginLeft = 0.4
 
-//DefaultMarginRight Right margin in inches. Defaults to 1cm (~0.4 inches).
+// DefaultMarginRight Right margin in inches. Defaults to 1cm (~0.4 inches).
 const DefaultMarginRight = 0.4
 
-//DefaultPageRanges Paper ranges to print, e.g., '1-5, 8, 11-13'. Defaults to the empty string, which means print all pages.
+// DefaultPageRanges Paper ranges to print, e.g., '1-5, 8, 11-13'. Defaults to the empty string, which means print all pages.
 const DefaultPageRanges = ""
 
-//DefaultIgnoreInvalidPageRanges Whether to silently ignore invalid but successfully parsed page ranges, such as '3-2'. Defaults to false.
+// DefaultIgnoreInvalidPageRanges Whether to silently ignore invalid but successfully parsed page ranges, such as '3-2'. Defaults to false.
 const DefaultIgnoreInvalidPageRanges = false
 
-//DefaultHeaderTemplate HTML template for the print header. Should be valid HTML markup with following classes used to inject printing values into them: - date: formatted print date - title: document title - url: document location - pageNumber: current page number - totalPages: total pages in the document  For example, <span class=title></span> would generate span containing the title.
+// DefaultHeaderTemplate HTML template for the print header. Should be valid HTML markup with following classes used to inject printing values into them: - date: formatted print date - title: document title - url: document location - pageNumber: current page number - totalPages: total pages in the document  For example, <span class=title></span> would generate span containing the title.
 const DefaultHeaderTemplate = ""
 
-//DefaultFooterTemplate HTML template for the print footer. Should use the same format as the headerTemplate.
+// DefaultFooterTemplate HTML template for the print footer. Should use the same format as the headerTemplate.
 const DefaultFooterTemplate = ""
 
-//DefaultPreferCSSPageSize Whether or not to prefer page size as defined by css. Defaults to false, in which case the content will be scaled to fit the paper size.
+// DefaultPreferCSSPageSize Whether or not to prefer page size as defined by css. Defaults to false, in which case the content will be scaled to fit the paper size.
 const DefaultPreferCSSPageSize = false
 
-//DefaultWaitingTime Waiting time after the page loaded. Default 0 means not wait. unit:Millisecond
+// DefaultWaitingTime Waiting time after the page loaded. Default 0 means not wait. unit:Millisecond
 const DefaultWaitingTime = 0
 
 // PDFParams print page as PDF.
@@ -63,7 +64,7 @@ type PDFParams struct {
 	WaitingTime int // Waiting time after the page loaded. Default 0 means not wait. unit:Millisecond
 }
 
-//NewDefaultPDFParams default pdf params
+// NewDefaultPDFParams default pdf params
 func NewDefaultPDFParams() PDFParams {
 	return PDFParams{
 		PrintToPDFParams: page.PrintToPDFParams{
@@ -108,15 +109,62 @@ func (ins *html2pdf) Convert() ([]byte, error) {
 	ctx, cancel := chromedp.NewContext(ins.ctx)
 	defer cancel()
 
-	if err := chromedp.Run(ctx,
+	firstPageWait := 2 * time.Second
+	lastPageWait := 2 * time.Second
+
+	err := chromedp.Run(ctx, chromedp.Tasks{
 		chromedp.Navigate(ins.cc.Url),
-		chromedp.Sleep(time.Duration(params.WaitingTime)*time.Millisecond),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			var err error
-			ins.buf, _, err = params.Do(ctx)
-			return err
-		}),
-	); err != nil {
+		chromedp.Sleep(firstPageWait),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Keep scrolling until you reach the bottom of the page
+	totalWaitTime := time.Duration(params.WaitingTime)*time.Millisecond - firstPageWait - lastPageWait
+	waitEachPage := 500 * time.Millisecond
+	for i := time.Duration(0); i < totalWaitTime; i += waitEachPage {
+		// Simulate pressing the Page Down key
+		err := chromedp.Run(ctx, chromedp.Tasks{chromedp.KeyEvent(kb.PageDown)})
+		if err != nil {
+			return nil, err
+		}
+
+		// Wait for a while to ensure that the page has finished loading
+		err = chromedp.Run(ctx, chromedp.Sleep(waitEachPage))
+		if err != nil {
+			return nil, err
+		}
+
+		// Check if the page has scrolled to the bottom
+		var isBottom bool
+		err = chromedp.Run(ctx,
+			chromedp.EvaluateAsDevTools(
+				// From https://techstacker.com/javascript-detect-when-scrolled-to-bottom/
+				`window.innerHeight + window.pageYOffset >= document.body.offsetHeight`,
+				&isBottom,
+			))
+		if err != nil {
+			return nil, err
+		}
+
+		// Exit the loop if the page has scrolled to the bottom
+		if isBottom {
+			break
+		}
+	}
+
+	err = chromedp.Run(ctx, chromedp.Sleep(lastPageWait))
+	if err != nil {
+		return nil, err
+	}
+
+	err = chromedp.Run(ctx, chromedp.ActionFunc(func(ctx context.Context) error {
+		var err error
+		ins.buf, _, err = params.Do(ctx)
+		return err
+	}))
+	if err != nil {
 		return nil, err
 	}
 
